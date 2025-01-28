42ca8229dd61c173917ff3bd427131a0cbd500f1
Schpidi <stephan@meissl.name>
2025-01-28 13:42:10 +0000

    Deploying to gh-pages from @ gtif-cerulean/deside-client@537f37896a1f0d59c40ff19ed32f7ebb8e93d66d 🚀

---
+++ assets/chunks/EodashStacInfo-_BfonNUG.C6PXsBvH.js
@@ -0,0 +1,16 @@
+import"./main.DJoFX4Ap.js";import{a_ as t}from"./eo-dash.CV0LDrfF.js";import{c as r,j as o,k as a,o as i}from"./framework.B8BIOpTZ.js";import"./lit-element.Cu4xVypU.js";import"./when.BR7zwNJC.js";import"./commonjsHelpers.BosuxZz1.js";import"./map.Co3hxIo-.js";import"./directive.CF8sV3Lr.js";import"./theme.DfgGqbCB.js";const l={class:"flex-grow-1 fill-height overflow-auto"},n=[".for",".allowHtml",".body",".featured",".footer",".styleOverride",".header",".subheader",".tags"],b={__name:"EodashStacInfo",props:{allowHtml:{type:Boolean,default:!0},styleOverride:{type:String,default:`
+.single-property {columns: 1!important;}
+h1 {margin:0px!important;font-size:16px!important;}
+header h1:after {
+content:' ';
+display:block;
+border:1px solid #d0d0d0;
+}
+h2 {font-size:15px}
+h3 {font-size:14px}
+summary {cursor: pointer;}
+#properties li > .value { font-weight: normal !important;}
+main {padding-bottom: 10px;}
+.footer-container {line-height:1;}
+.footer-container button {margin-top: -10px;}
+.footer-container small {font-size:10px;line-height:1;}`},header:{type:Array,default:()=>["title"]},tags:{type:Array,default:()=>["themes"]},subheader:{type:Array,default:()=>[]},body:{type:Array,default:()=>["satellite","sensor","agency","extent"]},featured:{type:Array,default:()=>["description","providers","assets","links"]},footer:{type:Array,default:()=>["sci:citation"]}},setup(e){return(s,d)=>(i(),r("div",l,[o("eox-stacinfo",{".for":a(t),".allowHtml":e.allowHtml,".body":e.body,".featured":e.featured,".footer":e.footer,".styleOverride":e.styleOverride,".header":e.header,".subheader":e.subheader,".tags":e.tags,style:{"--color-primary-lighter":"none"}},null,40,n)]))}};export{b as default};
