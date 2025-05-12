a5ca776dbde075769665733eecd5760f9884815a
silvester-pari <silvester.pari@eox.at>
2025-05-12 13:54:30 +0200

    chore: target _blank to all links

--- .vitepress/components/destine/DesideFooter.vue
+++ .vitepress/components/destine/DesideFooter.vue
@@ -26,22 +26,22 @@ onMounted(async () => {
 
       <!-- Central Container -->
       <div id="footer-central-container">
-        <a href="https://destination-earth.eu">
+        <a href="https://destination-earth.eu" target="_blank">
           <img class="footer-logo" alt="destination earth" src="/assets/destine/destination-earth.png">
         </a>
-        <a href="https://european-union.europa.eu/">
+        <a href="https://european-union.europa.eu/" target="_blank">
           <img class="footer-logo" alt="eu commission" src="/assets/destine/funded-by-EU.png">
         </a>
         <a>
           <img class="footer-logo" alt="implemented by" src="/assets/destine/implemented-by.png">
         </a>
-        <a href="https://www.ecmwf.int/">
+        <a href="https://www.ecmwf.int/" target="_blank">
           <img class="footer-logo" alt="ecmwf" src="/assets/destine/ecmwf.png">
         </a>
-        <a href="https://www.esa.int/">
+        <a href="https://www.esa.int/" target="_blank">
           <img class="footer-logo" alt="esa" src="/assets/destine/esa.png">
         </a>
-        <a href="https://www.eumetsat.int/">
+        <a href="https://www.eumetsat.int/" target="_blank">
           <img class="footer-logo" alt="eumetsat" src="/assets/destine/eumetsat.png">
         </a>
       </div>
@@ -54,11 +54,11 @@ onMounted(async () => {
 
     <!-- Footer menu -->
     <div id="footer-menu">
-      <a class="frame-link" href="https://platform.destine.eu/code-of-conduct/">Code of Conduct</a>
-      <a class="frame-link" href="https://platform.destine.eu/terms-and-conditions/">Terms and Conditions</a>
-      <a class="frame-link" href="https://platform.destine.eu/privacy-policies/">Privacy Policies</a>
-      <a class="frame-link" href="https://platform.destine.eu/cookie-policy/">Cookie Policy</a>
-      <a class="frame-link" href="https://platform.destine.eu/legal-notice/">Legal Notice</a>
+      <a class="frame-link" href="https://platform.destine.eu/code-of-conduct/" target="_blank">Code of Conduct</a>
+      <a class="frame-link" href="https://platform.destine.eu/terms-and-conditions/" target="_blank">Terms and Conditions</a>
+      <a class="frame-link" href="https://platform.destine.eu/privacy-policies/" target="_blank">Privacy Policies</a>
+      <a class="frame-link" href="https://platform.destine.eu/cookie-policy/" target="_blank">Cookie Policy</a>
+      <a class="frame-link" href="https://platform.destine.eu/legal-notice/" target="_blank">Legal Notice</a>
       <div class="mobile-link">
         <hr>
         <a class="footer-frame-link" href="https://destination-earth.eu">
