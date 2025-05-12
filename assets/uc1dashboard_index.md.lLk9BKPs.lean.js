e958fd9e89ec642cd243a5502ff0ff541613c1c7
santilland <daniel.santillan@eox.at>
2025-05-12 20:19:25 +0000

    Deploying to gh-pages from @ gtif-cerulean/deside-client@7172171de8f19e4222842d765755d1a71c7472de 🚀

---
+++ assets/uc1dashboard_index.md.lLk9BKPs.lean.js
@@ -0,0 +1,8 @@
+import{c as d,o as i,j as c,k as r,g as l}from"./chunks/framework.Bt_6m3-3.js";const p=["config"],m=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"page"},"headers":[],"relativePath":"uc1dashboard/index.md","filePath":"uc1dashboard/index.md"}'),f={name:"uc1dashboard/index.md"},_=Object.assign(f,{setup(h){return typeof window<"u"&&function(e,n){const s=setInterval(()=>{const t=document.querySelector(e);if(t&&t.shadowRoot){clearInterval(s);const o=document.createElement("style");o.textContent=n,t.shadowRoot.appendChild(o)}},100)}("eox-chart",`
+            input {
+                float: left !important;
+                padding: 0px !important;
+                margin: 0px !important;
+                border: none;
+            }
+        `),(a,e)=>(i(),d("div",null,[c("eo-dash",{style:{height:"calc(100dvh - 126px)",display:"flex",width:"100%"},config:r(l)("/configs/firstconfig.js")},null,8,p)]))}});export{m as __pageData,_ as default};
