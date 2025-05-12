5395e1a93ee99fa8c008d1192b0189ef5f596d94
santilland <daniel.santillan@eox.at>
2025-05-12 18:46:34 +0000

    Deploying to gh-pages from @ gtif-cerulean/deside-client@d45aba76e09040fd8f6a43695a91a32d36ae8c00 🚀

---
+++ assets/chunks/ssrBoot-BP7SYRyC.Dx4Y0jwe.js
@@ -0,0 +1,1 @@
+import{D as t,v as e,h as s,ay as n}from"./framework.Bt_6m3-3.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
